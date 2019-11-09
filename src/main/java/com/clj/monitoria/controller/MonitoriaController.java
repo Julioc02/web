34ba3f61f276358cbbe19a45aaca2cdc475d38d1
Julio Cesar <siqueiraj141@gmail.com>
2019-11-08 23:41:25 -0200

    v10(web)

--- src/main/java/com/clj/monitoria/controller/MonitoriaController.java
+++ src/main/java/com/clj/monitoria/controller/MonitoriaController.java
@@ -1,12 +1,16 @@
 package com.clj.monitoria.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.clj.monitoria.model.Aluno;
 import com.clj.monitoria.model.Monitores;
@@ -27,9 +31,20 @@ public class MonitoriaController {
    	 modelAndView.addObject(new Monitores());
    	 return modelAndView;
     }
+    
+    
+    
+    
+    
     @PostMapping("/monitores")
-    public String salvar(Monitores monitores) {
+    public String salvar(@Valid Monitores monitores, BindingResult result,
+    		RedirectAttributes attributes) {
+    	if(result.hasErrors()) {
+    		return "/(monitores)";
+    	}
+    
    	  monitor.save(monitores);
+   	  attributes.addFlashAttribute("mensagem", "Monitor salvo com sucesso!");	
    	  return "redirect:/";
     }
     
--- src/main/java/com/clj/monitoria/model/Monitores.java
+++ src/main/java/com/clj/monitoria/model/Monitores.java
@@ -3,14 +3,22 @@ package com.clj.monitoria.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Monitores {
 	@Id
 	@GeneratedValue
     public Long id;
+	@NotBlank(message = "Nome é obrigatório")
+	@Size(max = 100)
     private String nome;
-  	
+  	@NotBlank(message = "Informe o dia")
+  	@Size(max = 8)
   	private String dia;
 
   	
